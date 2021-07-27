f062ce4448980f4ecd16139a788ae1467392c611
Emre Cenk <emrecenk9@gmail.com>
2021-07-27 14:48:28 -0400

    created the circles and rectangle

--- venn_diagram.py
+++ venn_diagram.py
@@ -8,9 +8,9 @@ from manim import *
 class VennScene(Scene):
 
     def construct(self):
-        self.introduction()
-        self.wait(1)
-
+        # self.introduction()
+        # self.wait(1)
+        self.venn_animation()
     def introduction(self):
         video_name = Tex("Venn Diagram With 2 Sets")
         self.play(FadeIn(video_name, run_time = 2))
@@ -31,5 +31,23 @@ class VennScene(Scene):
             FadeOut(u_set_text, a_set_text, b_set_text, run_time = 2.5)
         )
 
+    def venn_animation(self):
+
+        #outer rectangle:
+        outer_rectangle = Rectangle().scale(2.5)
+        outer_rectangle.fill_color = YELLOW
+
+        circle_scale = 1.75
+        left_circle = Circle().scale(circle_scale)
+        right_circle = Circle().scale(circle_scale)
+        shift_coef = 1.5 * circle_scale
+
+        #a shift coefficient of 1.5 creates a perfect venn diagram
+
+        left_circle.shift(LEFT*shift_coef)
+        right_circle.shift(RIGHT*shift_coef)
+
+
+        self.play(FadeIn(right_circle, left_circle, outer_rectangle, run_time = 2))
 
 
