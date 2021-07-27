5f5a290e8c298d16aec370b392994d1d13bd69b9
Emre Cenk <emrecenk9@gmail.com>
2021-07-27 14:40:03 -0400

    finished introduction

--- venn_diagram.py
+++ venn_diagram.py
@@ -9,6 +9,7 @@ class VennScene(Scene):
 
     def construct(self):
         self.introduction()
+        self.wait(1)
 
     def introduction(self):
         video_name = Tex("Venn Diagram With 2 Sets")
@@ -16,13 +17,19 @@ class VennScene(Scene):
         self.wait(1)
         self.play(FadeOut(video_name, run_time = 1))
 
-        u_set_text = Tex("U = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}")
-        a_set_text = Tex("A = {1, 2, 3, 5, 8}").next_to(u_set_text, direction = DOWN)
-        b_set_text = Tex("B = {2, 4, 6, 8}").next_to(a_set_text, direction = DOWN)
+        u_set_text = MathTex("U = \{1, 2, 3, 4, 5, 6, 7, 8, 9, 10\}")
+        a_set_text = MathTex("A = \{1, 2, 3, 5, 8\}").next_to(u_set_text, direction = DOWN)
+        b_set_text = MathTex("B = \{2, 4, 6, 8\}").next_to(a_set_text, direction = DOWN)
 
         self.play(
             FadeIn(u_set_text, a_set_text, b_set_text, run_time = 2.5)
         )
 
+        self.wait(1)
 
-        self.wait(1.5)
+        self.play(
+            FadeOut(u_set_text, a_set_text, b_set_text, run_time = 2.5)
+        )
+
+
+
